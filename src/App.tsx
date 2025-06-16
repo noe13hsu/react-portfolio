af2be6f1221d4a88c453ceb47019224a528f8ace
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-16 11:37:28 +1000

    Complete site navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { Route, Routes } from 'react-router-dom'
+import { FaBriefcase, FaHome, FaInfo, FaMailBulk } from "react-icons/fa";
 
 import './App.css'
 import { About } from './pages/about'
 import { Home } from './pages/home'
+import { IconLink } from './components/icon-link'
+import { Projects } from './pages/projects';
+import { Contact } from './pages/contact';
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <div>
+      <nav className="">
+        <IconLink>
+          <FaHome />
+        </IconLink>
+
+        <IconLink to="/about">
+          <FaInfo />
+        </IconLink>
+
+        <IconLink to="/projects">
+          <FaBriefcase />
+        </IconLink>
+
+        <IconLink to="/contact">
+          <FaMailBulk />
+        </IconLink>
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
