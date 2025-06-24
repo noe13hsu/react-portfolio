718d60fa7df8c68b033d64cf6e81f4ecd09ec54f
Noe Hsu <noe13hsu@gmail.com>
2025-06-24 20:46:16 +1000

    Change about to skills and hobbies

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,11 @@
 import { SubHeader } from '../components/sub-header'
 import { Header } from '../components/header'
-import { TimeLine } from '../components/timeline/timeline'
 
 export const About = () => {
   return (
     <>
       <Header text='About' />
-      <SubHeader text='Experience & Education' />
-      <TimeLine />
+      <SubHeader text='Skills & Hobbies' />
     </>
   )
 }
