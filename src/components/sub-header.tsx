cf8d13d6d976be2d6754a732dfc35ef4a975372c
Noe Hsu <noe13hsu@gmail.com>
2025-06-24 16:37:46 +1000

    Update style

--- src/components/sub-header.tsx
+++ src/components/sub-header.tsx
@@ -1,6 +1,6 @@
 export const SubHeader = ({text} : {text: string}) => {
   return (
-    <h3 className='text-[var(--ice)]'>
+    <h3 className='text-[var(--ice)] text-6xl mt-4'>
       {text}
     </h3>
   )
