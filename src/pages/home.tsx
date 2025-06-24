c279e6b801d1b0404f28bc8a821c9cf64fb0f167
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-24 17:32:20 +1000

    Clean up

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,19 +11,19 @@ const messages = [
     Icon: LuFlame,
     message: <>I'm a passionate front end developer with <strong className='important'>4 years of experience in React and 1.5 years in TypeScript</strong>.</>
   },
-    {
+  {
     Icon: LuBrush,
     message: <>I've also worked with UI libraries such as <strong className='important'>Material UI, Bootstrap, and Tailwind</strong>, and have experience building components with plain CSS.</>
   },
-    {
+  {
     Icon: LuWrench,
     message: <>I thrive on building and improving meaningful applications - so far <strong className='important'>from revamping websites to developing a tracking system and a multiplayer poker game</strong>.</>
   },
-    {
+  {
     Icon: LuLightbulb,
     message: <>With a strong foundation in mathematics, I approach problem-solving with <strong className='important'>logical precision and creativity</strong>.</>
   },
-    {
+  {
     Icon: LuSparkle,
     message: <>Coding is something I genuinely enjoy as it gives me a strong feeling of accomplishment, and I'm always <strong className='important'>excited to bring cool ideas to life through clean, efficient code</strong>.</>
   },
