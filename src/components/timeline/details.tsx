e148981ae5c5fcb871026472b62e38d5894a58ed
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-24 16:38:19 +1000

    Add Timeline

---
+++ src/components/timeline/details.tsx
@@ -0,0 +1,16 @@
+import { TimelineEntry } from "./types"
+
+export const Details = ({item} : {item: TimelineEntry}) => {
+  const {info, institution, title, year} = item
+  const header = title ? `${institution} - ${title}` : institution
+
+  return (
+    <div className='relative bg-[var(--grey)] m-2 p-2 rounded-md'>
+        <h3 className='text-[var(--aqua)] font-bold text-xl'>{header}</h3>
+        <h4 className='font-bold'>{year}</h4>
+        <ul className='list-disc ml-4'>
+          {info.map(infoItem => <li key={infoItem}>{infoItem}</li>)}
+        </ul>
+    </div>
+  )
+}
---
+++ src/components/timeline/types.ts
@@ -0,0 +1,7 @@
+export type TimelineEntry = {
+  info: string[],
+  institution: string,
+  title?: string,
+  type: 'experience' | 'education',
+  year: string,
+}
