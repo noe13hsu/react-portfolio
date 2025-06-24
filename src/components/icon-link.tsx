26c8a5b426861ce3a25a40b8f4d216768cffcc95
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-24 16:37:36 +1000

    Use ripple animation on hover

--- src/components/icon-link.tsx
+++ src/components/icon-link.tsx
@@ -1,25 +1,25 @@
-import { IconType } from "react-icons"
-import { Link } from "react-router-dom"
+import { IconType } from 'react-icons'
+import { Link } from 'react-router-dom'
 
 export const IconLink = ({Icon, label, to} : {Icon: IconType, label: string, to: string}) => {
   const isExternal = /^https?:\/\//.test(to)
 
   const button = (
-    <button aria-label={label} className='bg-[var(--ice)] text-[var(--aqua)] text-4xl rounded-xl p-6 w-fit my-4'>
+    <button aria-label={label} className='bg-[var(--ice)] text-[var(--aqua)] text-4xl rounded-xl p-6 w-fit hover:animate-ripple'>
       <Icon />
     </button>
   )
 
   if (isExternal) {
     return (
-      <a href={to} target="_blank" rel="noopener noreferrer">
+      <a className='my-4 cursor-default' href={to} rel='noopener noreferrer' target='_blank' >
         {button}
       </a>
     )
   }
 
   return (
-    <Link to={to}>
+    <Link className='my-4 cursor-default' to={to}>
       {button}
     </Link>
   )
