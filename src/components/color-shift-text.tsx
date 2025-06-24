510b905ff71d72313fae4a27dccc5fe4919f59d3
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-24 16:35:36 +1000

    Remove ColorShiftText

--- src/components/color-shift-text.tsx
+++
@@ -1,9 +0,0 @@
-import { colorClassMap, TextColor } from "./types"
-
-export const ColorShiftText = ({color, text} : {color: TextColor, text: string}) => {
-  return (
-    <span className={`dark:text-[var(--grey)] ${colorClassMap[color]}`}>
-      {text}
-    </span>
-  )
-}
