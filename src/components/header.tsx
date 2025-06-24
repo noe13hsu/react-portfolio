2fc885a7a7249efeab993cb3f8ef7cbb572bcbf2
Noe Hsu <noe13hsu@gmail.com>
2025-06-24 16:36:45 +1000

    Change style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
-export const Header = ({text} : {text: string }) => {
+export const Header = ({text} : {text: string}) => {
   return (
-    <h1 className='text-xl text-[var(--aqua)] underline decoration-solid decoration-[var(--grey)]'>
+    <h1 className='font-bold text-8xl important mt-8'>
       {text}.
     </h1>
   )
