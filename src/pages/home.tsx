ef46beecde3afd028860f6768bebb07e035f47e0
Noe Hsu <noe13hsu@gmail.com>
2025-06-24 16:45:46 +1000

    Update Home by adding messages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,45 @@
-import { Header } from "../components/header"
+import { FaGripfire, FaMedapps, FaRegSmile, FaRegThumbsUp, FaTools } from 'react-icons/fa'
+
+import { HomeMessage } from '../components/home-message'
+import { Header } from '../components/header'
+import { SubHeader } from '../components/sub-header'
+
+const INTERVAL = 200
+const messages = [
+  {
+    Icon: FaGripfire,
+    message: <>I'm a passionate front end developer with <strong className='important'>4 years of experience in React and 1.5 years in TypeScript</strong>.</>
+  },
+    {
+    Icon: FaTools,
+    message: <>I've also worked with UI libraries such as <strong className='important'>Material UI, Bootstrap, and Tailwind</strong>, and have experience building components with plain CSS.</>
+  },
+    {
+    Icon: FaMedapps,
+    message: <>I thrive on building and improving meaningful applications - so far <strong className='important'>from revamping websites to developing a tracking system and a multiplayer poker game</strong>.</>
+  },
+    {
+    Icon: FaRegThumbsUp,
+    message: <>With a strong foundation in mathematics, I approach problem-solving with <strong className='important'>logical precision and creativity</strong>.</>
+  },
+    {
+    Icon: FaRegSmile,
+    message: <>Coding is something I genuinely enjoy as it gives me a strong feeling of accomplishment, and I'm always <strong className='important'>excited to bring cool ideas to life through clean, efficient code</strong>.</>
+  },
+]
 
 export const Home = () => {
-    return <Header text='Home' />
+  return (
+    <>
+      <Header text='Noe Hsu' />
+      <SubHeader text='I am a React developer.' />
+      <div className='flex-1 flex flex-col text-[var(--ice)] text-2xl mt-40'>
+        {messages.map(({Icon, message}, index) => (
+          <HomeMessage delay={INTERVAL * (index + 1)} Icon={Icon}>
+            {message}
+          </HomeMessage>
+        ))}
+      </div>
+    </>
+  )
 }
