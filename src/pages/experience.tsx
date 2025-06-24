97791602b7a6885c12a7b05d18609299be1db316
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-24 20:46:41 +1000

    Change projects to experience

---
+++ src/pages/experience.tsx
@@ -0,0 +1,13 @@
+import { SubHeader } from '../components/sub-header'
+import { Header } from '../components/header'
+import { TimeLine } from '../components/timeline/timeline'
+
+export const Experience = () => {
+  return (
+    <>
+      <Header text='Experience' />
+      <SubHeader text='Work & Education' />
+      <TimeLine />
+    </>
+  )
+}
--- src/pages/projects.tsx
+++
@@ -1,5 +0,0 @@
-import { Header } from "../components/header"
-
-export const Projects = () => {
-    return <Header text='Projects' />
-}
