8637eec5fd2eb4569e54f3519fa3d1ccc912a75b
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-25 21:50:28 +1000

    Add contact form

---
+++ src/components/contact/input-field.tsx
@@ -0,0 +1,36 @@
+export const InputField = ({
+  autoComplete,
+  className = '',
+  id,
+  isFieldSet = false,
+  label,
+  required = true,
+  type = 'text',
+} : {
+  autoComplete?: string,
+  className?: string,
+  id: string,
+  isFieldSet?: boolean,
+  label: string,
+  required?: boolean,
+  type?: string,
+}) => {
+  return (
+    <div className={`flex ${isFieldSet ? 'flex-col-reverse' : 'flex-col'}`}>
+      <label
+        className={`text-[var(--${isFieldSet ? 'ice' : 'aqua'})] text-${isFieldSet ? 'sm' : '2xl'} ${className}`}
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <input
+        autoComplete={autoComplete}
+        className='bg-[var(--ice)] rounded-xs'
+        id={id}
+        name={id}
+        required={required}
+        type={type}
+      />
+    </div>
+  )
+}
