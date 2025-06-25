4ee4af7b4f99bb0e755763d6f8209a81cc399b9a
Noe Hsu <noe13hsu@gmail.com>
2025-06-25 15:33:13 +1000

    Apply FadeIn

--- src/components/experience/timeline-item.tsx
+++ src/components/experience/timeline-item.tsx
@@ -1,17 +1,27 @@
+import { FadeIn } from '../fade-in'
 import { VerticalBar } from '../vertical-bar'
 import { Details } from './details'
 import { HorizontalBar } from './horizontal-bar'
 import { TimelineEntry } from './types'
 
-export const TimelineItem = ({item} : {item: TimelineEntry}) => {
+const DETAILS_BASE_DELAY = 600
+const BAR_INITIAL_DELAY = 200
+
+export const TimelineItem = ({index, item} : {index: number, item: TimelineEntry}) => {
   const {type} = item
+  const detailsDelay = DETAILS_BASE_DELAY * (index + 1)
+  const barDelay = BAR_INITIAL_DELAY + DETAILS_BASE_DELAY * index
 
   return (
     <div className='grid grid-cols-[1fr_4%_auto_4%_1fr] text-[var(--ice)]'>
       {type === 'experience' && (
         <>
-          <Details item={item} />
-          <HorizontalBar />
+          <FadeIn delay={detailsDelay}>
+            <Details item={item} />
+          </FadeIn>
+          <FadeIn delay={barDelay} className='flex'>
+            <HorizontalBar />
+          </FadeIn>
         </>
       )}
 
@@ -21,8 +31,12 @@ export const TimelineItem = ({item} : {item: TimelineEntry}) => {
 
       {type === 'education' && (
         <>
-          <HorizontalBar />
-          <Details item={item} />
+          <FadeIn delay={barDelay} className='flex'>
+            <HorizontalBar />
+          </FadeIn>
+          <FadeIn delay={detailsDelay}>
+            <Details item={item} />
+          </FadeIn>
         </>
       )}
     </div>
