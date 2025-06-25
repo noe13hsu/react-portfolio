aff2f201eaa444ebc1fa1f9257c31f158d8c2a2e
Noe Hsu <noe13hsu@gmail.com>
2025-06-25 21:50:28 +1000

    Add contact form

---
+++ src/components/contact/form.tsx
@@ -0,0 +1,34 @@
+import { InputField } from './input-field'
+
+export const Form = () => {
+  return (
+    <form
+      action='https://formsubmit.co/noe.hsu@gmail.com'
+      className='bg-[var(--grey)] p-6 rounded-lg space-y-4 mt-10 w-120'
+      method='POST'
+    >
+      <fieldset>
+        <legend className='text-[var(--aqua)] text-2xl'>Name</legend>
+        <div id='name-field-set' className='grid grid-cols-2 gap-4'>
+          <InputField autoComplete='given-name' className='self-end' id='first-name' isFieldSet label='First Name' />
+          <InputField autoComplete='family-name' className='self-end' id='last-name' isFieldSet label='Last Name' />
+        </div>
+      </fieldset>
+
+      <InputField autoComplete='given-name' id='organisation' label='Organisation' required={false} />
+      <InputField autoComplete='email' id='email' label='Email' type='email' />
+      <InputField id='subject' label='Subject' />
+
+      <div className='flex flex-col'>
+        <label className='text-[var(--aqua)] text-2xl' htmlFor='message'>Message</label>
+        <textarea className='bg-[var(--ice)] h-40 rounded-xs' id='message' name='message' required />
+      </div>
+
+      <div className='flex justify-center'>
+        <button className='px-6 py-2 bg-[var(--aqua)] hover:animate-ripple rounded-sm' id='send-button' type='submit'>
+          Send
+        </button>
+      </div>
+    </form>
+  )
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,13 @@
+import { SubHeader } from '../components/sub-header'
 import { Header } from '../components/header'
+import { Form } from '../components/contact/form'
 
 export const Contact = () => {
-    return <Header text='Contact' />
+  return (
+    <>
+      <Header text='Contact' />
+      <SubHeader text={'Let\'s connect'} />
+      <Form />
+    </>
+  )
 }
