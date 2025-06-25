7d5f3e26fe70a094794c74a4001763576c8e75cf
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-25 11:52:34 +1000

    Use global grey

--- src/components/timeline/details.tsx
+++ src/components/timeline/details.tsx
@@ -5,7 +5,7 @@ export const Details = ({item} : {item: TimelineEntry}) => {
   const header = title ? `${institution} - ${title}` : institution
 
   return (
-    <div className='relative bg-gray-600 m-2 p-4 rounded-md'>
+    <div className='relative bg-[var(--grey)] m-2 p-4 rounded-md'>
         <h3 className='text-[var(--aqua)] font-bold text-xl'>{header}</h3>
         <h4 className='font-bold'>{year}</h4>
         <ul className='list-disc ml-4'>
