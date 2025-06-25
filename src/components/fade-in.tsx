e35b2a925f5264185106ad9ae87410ab5ec66730
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-25 15:32:24 +1000

    Rename to FadeIn

---
+++ src/components/fade-in.tsx
@@ -0,0 +1,17 @@
+import { ReactNode, useEffect, useState } from 'react'
+
+export const FadeIn = ({children, className = '', delay} : {children: ReactNode, className?: string, delay: number}) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsVisible(true), delay)
+
+    return () => clearTimeout(timeout)
+  })
+
+  return (
+    <div className={`transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'opacity-0'} ${className}`}>
+      {children}
+    </div>
+  )
+}
