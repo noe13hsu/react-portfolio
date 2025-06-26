b0aff885e19457f0989dbe2c1c5b21b460e1139c
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-26 23:01:21 +1000

    Add Nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import './App.css'
 import { IconLink } from './components/icon-link'
 import { VerticalBar } from './components/vertical-bar'
 import { Header } from './components/header'
-import { externalLinks, internalLinks } from './routes'
+import { externalLinks } from './routes'
 import { Main } from './components/main'
+import { Nav } from './components/nav'
 
 function App() {
   return (
     <div className='grid grid-rows-[auto_auto_6fr_auto_auto] grid-cols-1 h-[100dvh] lg:h-screen lg:grid-cols-[25%_1fr_25%] lg:grid-rows-[14%_1fr_6%]'>
-      <nav className='flex justify-between items-center m-2 gap-4 lg:m-4 lg:col-start-1 lg:row-start-1 lg:row-span-3 lg:flex-col '>
-        {internalLinks.map(({download, Icon, label, to}) => (
-          <IconLink key={label} download={download} Icon={Icon} label={label} to={to} />
-        ))}
-        <VerticalBar />
-      </nav>
-
+      <Nav />
       <Header />
       <Main />
 
