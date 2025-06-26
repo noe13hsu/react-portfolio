2566cd6e4bcd49fea7c842ac65a767204de778d0
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-26 23:21:34 +1000

    Clean up

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,7 +4,7 @@ import { IconLink } from './icon-link'
 
 export const Nav = () => {
   return (
-    <nav className='flex justify-between items-center m-2 gap-4 lg:m-4 lg:col-start-1 lg:row-start-1 lg:row-span-3 lg:flex-col '>
+    <nav className='flex justify-between items-center m-2 gap-4 lg:m-4 lg:col-start-1 lg:row-start-1 lg:row-span-3 lg:flex-col'>
       {internalLinks.map(({download, Icon, label, to}) => (
         <IconLink key={label} download={download} Icon={Icon} label={label} to={to} />
       ))}
