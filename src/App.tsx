f4394bd3f47c570e5665d15f58e85f88ecf23fc7
Noe Hsu <noe.hsu@codeworx.com.au>
2025-06-26 23:58:02 +1000

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Nav } from './components/nav'
 
 function App() {
   return (
-    <div className='grid grid-rows-[auto_auto_6fr_auto_auto] grid-cols-1 h-[100dvh] lg:h-screen lg:grid-cols-[25%_1fr_25%] lg:grid-rows-[14%_1fr_6%]'>
+    <div className='bg-[var(--navy)] grid grid-rows-[auto_auto_6fr_auto_auto] grid-cols-1 h-[100dvh] lg:h-screen lg:grid-cols-[25%_1fr_25%] lg:grid-rows-[14%_1fr_6%]'>
       <Nav />
       <Header />
       <Main />
