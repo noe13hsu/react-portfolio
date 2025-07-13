3ac40b688277f3e8f7c90743cedd18ddeec477e9
Noe Hsu <noe13hsu@gmail.com>
2025-07-14 00:07:33 +1000

    Refactor about

---
+++ src/components/about/hobbies.tsx
@@ -0,0 +1,19 @@
+import { FadeIn } from 'components/fade-in'
+import { Badge } from './badge'
+
+const hobbies = [
+  'Gaming',
+  'Movies',
+  'Music',
+  'Basketball',
+  'Baseball',
+  'Gundam model kits'
+]
+
+export const Hobbies = () => {
+  return (
+    <FadeIn delay={600} className='about-section-layout'>
+      {hobbies.map(hobby => <Badge key={hobby} className='text-[var(--ice)]' text={hobby} />)}
+    </FadeIn>
+  )
+}
