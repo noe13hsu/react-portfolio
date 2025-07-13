c032a77b0e32a0d727aec3b47bfd957753aa5ae2
Noe Hsu <noe13hsu@gmail.com>
2025-07-14 00:36:23 +1000

    Add 404

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { internalLinks } from '../routes'
+import { NotFound } from '../pages/not-found'
 
 export const Main = () => {
   return (
     <main className='main-layout'>
       <Routes>
         {internalLinks.map(({element, to}) => <Route element={element} path={to} />)}
+        <Route element={<NotFound />} path={'*'} />
       </Routes>
     </main>
   )
